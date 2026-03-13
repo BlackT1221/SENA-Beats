38eab386437333afbf3d7ef1922dc2c6a7962cc3
BlackT1221 <cdmartinez1121@gmail.com>
2026-03-13 15:06:24 -0500

    Cambio de titulo

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class SenaBeatsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'SENA Beats',
+      title: 'SENA Canciones',
 
       theme: ThemeData(
         brightness: Brightness.dark,
